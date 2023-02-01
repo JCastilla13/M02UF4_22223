f8b7b5fac78c64d62a5b55ef5341c058a594ef19
CastillaJesus <jesus.castilla@enti.cat>
2023-02-01 11:18:19 +0100

    First node test

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/bin/node
+
+//#!/usr/bin/env node
+
+
+
+const http = require('http');
+
+http.createServer(function(request, result){
+	console.log("Alguien se conecta");
+	result.write('ola k ase');
+	result.end();
+}).listen(8080);
